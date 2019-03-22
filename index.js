ce2ae8af3d6cff2b05da4ec98cc74365ce969811
faelplg <faelplg@gmail.com>
2019-03-22 16:54:33 -0300

    7 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// Import theme stylesheets
-import './main.scss';
+// Import application style
+import './theme/theme.scss';
 
-// Write Javascript code!
-const appDiv = document.getElementById('sidebar-grid__content');
-appDiv.innerHTML = `<h1>Components catalog</h1>`;
+// Import components
+import './components/components';
