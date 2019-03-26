9960806a22c2ab295eb2fee42942034031809483
faelplg <faelplg@gmail.com>
2019-03-26 15:32:28 -0300

    1 file modified

--- components/components.js
+++ components/components.js
@@ -1,5 +1,5 @@
 import './link/link.component';
 import './button/button.component';
-import './drawer/drawer.component';
+// import './drawer/drawer.component';
 import './top-app-bar/top-app-bar.component';
 import './tab-bar/tab-bar.component';
