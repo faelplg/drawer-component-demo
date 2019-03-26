66043fcc9a1f0eb44a532b5e3b55665b6a0754fe
faelplg <faelplg@gmail.com>
2019-03-26 15:20:00 -0300

    5 files modified, 2 added

--- components/components.js
+++ components/components.js
@@ -1,4 +1,5 @@
 import './link/link.component';
 import './button/button.component';
+import './tab-bar/tab-bar.component';
 import './top-app-bar/top-app-bar.component';
 import './drawer/drawer.component';
