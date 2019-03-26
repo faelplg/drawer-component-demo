f702c3a247333c9a99cf2ed4c1c7ddd804862cb3
faelplg <faelplg@gmail.com>
2019-03-25 21:13:29 -0300

    6 files modified, 2 added

--- components/components.js
+++ components/components.js
@@ -1,3 +1,4 @@
 import './link/link.component';
 import './button/button.component';
 import './top-app-bar/top-app-bar.component';
+import './drawer/drawer.component';
--- index.js
+++ index.js
@@ -4,5 +4,17 @@ import './theme/theme.scss';
 // Import components
 import './components/components';
 
+import {MDCTopAppBar} from "@material/top-app-bar";
+import {MDCDrawer} from "@material/drawer";
+
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('main__top-app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main__container'));
+
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  drawer.open = !drawer.open;
+});
+
 const test = document.getElementById('showroom__box');
 console.log('Testing', test);
