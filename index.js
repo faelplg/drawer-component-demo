ec0e5432257ccd2d8863c9cd537f4bfbc77d0713
faelplg <faelplg@gmail.com>
2019-03-28 19:47:34 -0300

    3 files modified

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ import TabBar from './components/tab-bar/tab-bar.component';
 
 const tabBar = new TabBar('.mdc-tab-bar');
 tabBar.component.focusOnActivate = false;
+tabBar.component.listen('MDCTabBar:activated', sig => {
+  console.log('Tab Bar event', sig);
+  updatePanel(sig.detail.index);
+});
 
 const topAppBar = new TopAppBar('main__top-app-bar');
 topAppBar.component.setScrollTarget(document.getElementById('main__container'));
@@ -20,6 +24,19 @@ topAppBar.component.listen('MDCTopAppBar:nav', () => {
 });
 
 const list = new List('.mdc-list');
+
+const panels = document.querySelector('.panels');
+
+function updatePanel(index) {
+  const activePanel = panels.querySelector('.panel.active');
+  if (activePanel) {
+    activePanel.classList.remove('active');
+  }
+  const newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
+  if (newActivePanel) {
+    newActivePanel.classList.add('active');
+  }
+}
 
 const test = document.getElementById('showroom__box');
 console.log('Testing', test);
